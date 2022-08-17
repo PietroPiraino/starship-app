766cff090becff93e1a60dc0d3434deb7323e25d
pietropiraino <pietropiraino91@gmail.com>
2022-08-17 16:18:10 +0200

    adding fetch call swapi API, loader and error handler.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
 
-  return (
-    <>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </>
-  );
+    const [movies, setMovies] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const fetchMoviesHandler = async () => {
+        setIsLoading(true)
+        setError(null)
+
+        try {
+            const response = await fetch('https://swapi.dev/api/films/')
+            if (!response.ok) {
+                throw new Error("Something went wrong!")
+            }
+            const data = await response.json()
+
+            const transformedData = data.results.map(movieData => ({
+                id: movieData.episode_id,
+                title: movieData.title,
+                openingText: movieData.opening_crawl,
+                releaseDate: movieData.release_date
+            }))
+            setMovies(transformedData)
+        } catch (error) {
+            setError(error.message)
+        }
+        setIsLoading(false)
+    }
+
+    return (
+        <>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+                {!isLoading && !error && movies.length === 0 && <p>No movies to display</p>}
+                {isLoading && <p>Loading..</p>}
+                {!isLoading && error && <p>{error}</p>}
+            </section>
+        </>
+    );
 }
 
 export default App;
